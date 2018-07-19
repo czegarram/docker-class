3a41ddc3f4365c8aa2531b7ace78f729beae75e4
Ubuntu <dockeradmin@ServerDocker.l3fpvxswxgverjilyzi4synsfe.jx.internal.cloudapp.net>
2018-07-19 02:41:11 +0000

    new php file

--- mongodb/nginx/www/app/index.php
+++ mongodb/nginx/www/app/index.php
@@ -1,6 +1,14 @@
 <?php
   
-  echo "Hola";
-  phpinfo();
+  
+ $connection = pg_connect ("host=postgresql-app dbname=app user=root password=root");
+    if($connection) {
+	           echo 'connected to postgresql :)';
+		       } else {
+			               echo 'there has been an error connecting';
+				           } 
+
+
+phpinfo();
 
 ?>
